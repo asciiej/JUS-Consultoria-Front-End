ba066fc66934bdf2d15e394dd992035562148fdb
Lucas Janot <lucas.janot@ufu.br>
2024-05-01 17:11:25 -0600

    Adicionando o esqueleto dos models

---
+++ src/classes/contrato/contratoModel.py
@@ -0,0 +1,59 @@
+import usuario.usuarioModel as usuario
+class contratoModel:
+  
+    def init(self,titulo:str,tipo:str,valor:str,formaDePagamento:str,multaEncargos:str,prazoDuracao:str):
+        self.titulo = titulo
+        self.tipo = tipo
+        self.valor = valor
+        self.formaDePagamento = formaDePagamento
+        self.multaEncargos = multaEncargos
+        self.prazoDuracao = prazoDuracao
+
+    def str(self)->str:
+         return (f"Titulo: {self.titulo}\n"
+                f"Tipo: {self.tipo}\n"
+                f"Valor: {self.valor}\n"
+                f"FormaDePagamento: {self.formaDePagamento}\n"
+                f"MultaEncargos: {self.multaEncargos}\n"
+                f"PrazoDuracao: {self.prazoDuracao}\n")
+    
+
+class campoDeUmContrato:
+
+    def init(self,tipo:str,texto:str):
+        self.tipo = tipo
+        self.texto = texto
+
+    def str(self)->str:
+        return(f"Texto: {self.texto}")
+
+
+class parteDeUmContrato:
+    def init(self,nomeCompleto:str,nacionalidade:str,estadoCivil:str,profissao:str,cpf:str,cnjp:str,endereco:str):
+        self.nomeCompleto = nomeCompleto
+        self.nacionalidade = nacionalidade
+        self.estadoCivil = estadoCivil
+        self.profissao = profissao
+        self.cpf = cpf
+        self.cnpj = cnjp
+        self.endereco = endereco
+
+    def str(self)->str:
+         return (f"NomeCompleto: {self.nomeCompleto}\n"
+                f"Nacionalidade: {self.nacionalidade}\n"
+                f"EstadoCivil: {self.estadoCivil}\n"
+                f"Profissao: {self.profissao}\n"
+                f"CPF: {self.cpf}\n"
+                f"CNPJ: {self.cnpj}\n"
+                f"Endereco: {self.endereco}")
+
+
+class contratoIndividualModel (contratoModel): 
+
+    def init(self,titulo:str,tipo:str,valor:str,formaDePagamento:str,multaEncargos:str,prazoDuracao:str,usuario:usuario,camposDeUmContrato:list,partesDeUmContrato:list):
+        super().init(titulo,tipo,valor,formaDePagamento,multaEncargos,prazoDuracao)
+        self.usuario = usuario
+        self.camposDeUmcontrato = camposDeUmContrato
+        self.partesDeUmContrato = partesDeUmContrato
+
+    
