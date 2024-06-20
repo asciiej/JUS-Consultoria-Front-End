eebbf7e02fad051816a88723db2400841bda491b
Lucas Janot <lucas.janot@ufu.br>
2024-06-20 08:15:41 -0600

    GitIgnore Atualizado

--- src/interface/checagemInformacoes.py
+++ src/interface/checagemInformacoes.py
@@ -1,7 +1,8 @@
+from src.interface.assinaturaDocumento import telaAssinaturaDocumento
 
 class checagemInformacoes():
     def __init__(self,id:int,tipo:str,título:str,controlers:dict):
-        retornoBD = controlers['contract'].modeloDeContrato().get_by_title("Contrato de Prestacao de Servicos Profissionais")
+        retornoBD = controlers['contract'].modeloDeContrato().get_by_title(título)
         print(retornoBD)
 
         if tipo == "Consultoria Empresarial":
@@ -10,6 +11,8 @@ class checagemInformacoes():
             self.consultoria_tributaria()
         elif tipo == "Câmara de Arbitragem":
             self.camara_arbitragem()
+
+        #telaAssinaturaDocumento
 
     def consultoria_empresarial(self):
         print("Checando dados empresarial")
